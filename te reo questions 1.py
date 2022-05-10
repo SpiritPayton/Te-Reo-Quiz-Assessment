f981f810a298a3b18bf359031ee7b59b8618f17d
Spirit Payton <98915494+SpiritPayton@users.noreply.github.com>
2022-05-11 10:12:33 +1200

    Update te reo questions 1.py

--- te reo questions 1.py
+++ te reo questions 1.py
@@ -3,12 +3,30 @@ the English word meaning the same as one of the Te Reo words.
 """
 
 
-# answers
-"bread" = "paraoa"
-"cheese" = "tīhi"
-"fish" = "ika"
-"fruits" = "huarākau"
+# answers (I'm not yet exactly sure how to make them mean the same thing)
+"bread" == "paraoa"
+"cheese" == "tīhi"
+"fish" == "ika"
+"fruits" == "huarākau"
+"salad" == "huamata"
+"meat" == "mīti"
+"soup" == "hupa"
+"coffee" == "kawhe"
+"egg" == "hēki"
+"coconut water" == "wai kokonati"
+
+english_word = 0
+maori_word = 0
+
+
+# asking the questions and getting input (Not good though)
+while True:
+    d1a = input("Maori words: \n A) [maori word 1]. B) [maori word 2]. [A/B]? : ").lower()
+    if d1a == "a":
+        print("next question")
+    elif d1a == "b":
+        print("wrong. next question")
+        break
 
 
 # Main routine goes here
-
