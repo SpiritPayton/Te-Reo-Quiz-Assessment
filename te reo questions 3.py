2ddddd84c211f5c156097ac8fecd01185c1277b6
Spirit Payton <98915494+SpiritPayton@users.noreply.github.com>
2022-05-18 10:30:33 +1200

    Update te reo questions 3.py

--- te reo questions 3.py
+++ te reo questions 3.py
@@ -1,4 +1,5 @@
-"""function to ask the quiz questions AND track score
+"""function to ask the quiz questions AND display score at end
+no randomisation of questions & answers yet
 """
 
 
@@ -10,13 +11,26 @@ class Question:
 
 question_prompts = [
     "What is the Maori word for bread? \na. paraoa \nb. ika \nc. hupa\n\n",
-    "What is the Maori word for bread? \na. paraoa \nb. ika \nc. hupa\n\n",
-    "What is the Maori word for bread? \na. paraoa \nb. ika \nc. hupa\n\n"]
+    "What is the Maori word for meat? \na. kawhe \nb. huamata \nc. mīti\n\n",
+    "What is the Maori word for fruits? \na. huarākau \nb. hēki \nc. ika\n\n"]
+
+
+"bread" == "paraoa"
+"cheese" == "tīhi"
+"fish" == "ika"
+"fruits" == "huarākau"
+"salad" == "huamata"
+"meat" == "mīti"
+"soup" == "hupa"
+"coffee" == "kawhe"
+"egg" == "hēki"
+"coconut water" == "wai kokonati"
+
 
 questions = [
     Question(question_prompts[0], "a"),
     Question(question_prompts[1], "c"),
-    Question(question_prompts[3], "b")]
+    Question(question_prompts[2], "a")]
 
 
 def run_test(questions):
@@ -25,7 +39,7 @@ def run_test(questions):
         answer = input(question.prompt)
         if answer == question.answer:
             score += 1
-    print("You got " + str(score) + "/" + str(len(questions)) + "Correct")
+    print("You got " + str(score) + "/" + str(len(questions)) + " Correct")
 
 
 run_test(questions)
